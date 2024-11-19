57c218cffd143de21a49da52b469354f5d503350
Aiden Baker <baker.aiden@protonmail.com>
2024-11-19 16:48:23 -0500

    Sprite import, drawing, accessory structs

--- source/main.c
+++ source/main.c
@@ -30,7 +30,20 @@
 
 #define TOP_SCREEN_WIDTH 400
 #define TOP_SCREEN_HEIGHT 240
+#define BOTTOM_SCREEN_WIDTH 320
+#define BOTTOM_SCREEN_HEIGHT 240
+
 #define MAX_TEXT_SIZE 1024 * 16
+#define MAX_SPRITES 256 //arbitrary
+
+typedef struct
+{
+	C2D_Sprite sprite;
+	bool pressed;
+} UIButton;
+
+static C2D_SpriteSheet spriteSheet;
+
 
 void drawText(int x, int y, int z, float scale, u32 color, char* text, int flags, C2D_Font font) {
     C2D_Text drawText;
@@ -50,6 +63,18 @@ int main(int argc, char **argv)
     cfguInit();
     gfxInitDefault();
     atexit(gfxExit);
+
+    // Load graphics
+	spriteSheet = C2D_SpriteSheetLoad("romfs:/gfx/sprites.t3x");
+	if (!spriteSheet) svcBreak(USERBREAK_PANIC);
+
+	UIButton buttonMain;
+	buttonMain.pressed = 0;
+	C2D_SpriteFromSheet(&buttonMain.sprite, spriteSheet, 0);
+	buttonMain.sprite.params.pos.x = 38;
+	buttonMain.sprite.params.pos.y = 20;
+
+
     C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
     C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
     C2D_Prepare();
@@ -65,25 +90,49 @@ int main(int argc, char **argv)
 
 	while (aptMainLoop())
 	{
-		//Scan all the inputs. This should be done once for each frame
-		hidScanInput();
-		hidTouchRead( &touch );
+        // Pre-processing for frame
+        hidScanInput();
+        hidTouchRead( &touch );
 
+        u32 kDown = hidKeysDown();
+		if (kDown & KEY_START) break; // break in order to return to hbmenu
+
+
+		// Top Screen
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
         C2D_TargetClear(top, backgroundColor);
         C2D_SceneBegin(top);
-
         char randomData[100];
         snprintf(randomData, sizeof(randomData), "(%d, %d)", touch.px, touch.py);
         drawText(6, 6, 1, 1, textColor, randomData, C2D_WithColor | C2D_WordWrap, font);
 
-		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START) break; // break in order to return to hbmenu
 
+        // Bottom Screen
+        C2D_TargetClear(bottom, backgroundColor);
+        C2D_SceneBegin(bottom);
+        C2D_DrawSprite(&buttonMain.sprite);
 
 		C3D_FrameEnd(0);
 	}
 	C2D_FontFree(font);
 	exit(0);
+}
+
+void failExit(const char *fmt, ...) {
+    va_list ap;
+
+    printf("\x1b[31;1m"); // Set color to red
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    printf("\x1b[0m"); // Reset color
+    printf("\nPress B to exit\n");
+
+    while (aptMainLoop()) {
+        gspWaitForVBlank();
+        hidScanInput();
+
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_B) exit(0);
+    }
 }
