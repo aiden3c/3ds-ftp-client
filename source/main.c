2a75e4e7a7481c77dcb778c9b0f29605451f4935
Aiden Baker <baker.aiden@protonmail.com>
2024-11-19 19:31:15 -0500

    Better button, better indicators

--- source/main.c
+++ source/main.c
@@ -23,6 +23,7 @@
 #include <mbedtls/debug.h>
 
 #include <curl/curl.h>
+#include <math.h>
 
 #include <citro2d.h>
 
@@ -40,10 +41,26 @@ typedef struct
 {
 	C2D_Sprite sprite;
 	bool pressed;
+	bool disabled;
+	bool rainbow;
+	char* text;
+	int textOffsetX;
+	int textOffsetY;
+	float textScale;
+	C2D_Font* font;
 } UIButton;
 
-static C2D_SpriteSheet spriteSheet;
+typedef struct
+{
+    int rainbowDelay;
+    int rainbowDelayDefault;
+} AppState;
 
+static C2D_SpriteSheet spriteSheet;
+AppState mainState;
+C2D_ImageTint rainbowTint;
+C2D_ImageTint shadowTint;
+C2D_ImageTint disabledTint;
 
 void drawText(int x, int y, int z, float scale, u32 color, char* text, int flags, C2D_Font font) {
     C2D_Text drawText;
@@ -57,12 +74,105 @@ void drawText(int x, int y, int z, float scale, u32 color, char* text, int flags
     C2D_TextBufDelete(dynamicBuffer);
 }
 
+u32 hueToRGB(float hue) {
+    float r = fabsf(hue * 6.0f - 3.0f) - 1.0f;
+    float g = 2.0f - fabsf(hue * 6.0f - 2.0f);
+    float b = 2.0f - fabsf(hue * 6.0f - 4.0f);
+
+    r = fminf(fmaxf(r, 0.0f), 1.0f);
+    g = fminf(fmaxf(g, 0.0f), 1.0f);
+    b = fminf(fmaxf(b, 0.0f), 1.0f);
+
+    return C2D_Color32((u8)(r * 255), (u8)(g * 255), (u8)(b * 255), 0xFF);
+}
+
+void updateRainbowTint(C2D_ImageTint* tint, AppState* appState) {
+    static float hue = -1.0f;
+
+    // Initialize the hue randomly on the first call
+    if (hue < 0.0f) {
+        srand((unsigned int)time(NULL)); // Seed the random generator
+        hue = (float)rand() / RAND_MAX; // Random value between 0.0 and 1.0
+    }
+
+    if (appState->rainbowDelay > 0) {
+        appState->rainbowDelay--;
+        return;
+    }
+    appState->rainbowDelay = appState->rainbowDelayDefault;
+
+    hue += 0.01f;
+    if (hue >= 1.0f) hue -= 1.0f;
+
+    u32 color = hueToRGB(hue);
+
+    for (int i = 0; i < 4; i++) {
+        tint->corners[i].color = color;
+        tint->corners[i].blend = 1.0f;
+    }
+}
+
+void drawButton(UIButton* button) {
+    int distance = 4;
+    bool needShadow = 1;
+
+    if(button->pressed == 0 ^ button->disabled == 0) { // Raise button for rendering to indicate pressability
+        needShadow = 0;
+    }
+
+    if(needShadow) {
+        C2D_DrawSpriteTinted(&button->sprite, &shadowTint);
+        button->sprite.params.pos.x -= distance;
+        button->sprite.params.pos.y -= distance;
+    }
+
+    if(button->rainbow != 0 && button->disabled == 0)
+    {
+        C2D_DrawSpriteTinted(&button->sprite, &rainbowTint);
+    } else {
+        C2D_DrawSprite(&button->sprite);
+    }
+
+    drawText(button->sprite.params.pos.x + button->textOffsetX, button->sprite.params.pos.y + button->textOffsetY, 0, button->textScale, C2D_Color32(0xFB, 0xFC, 0xFC, 0xFF), button->text, C2D_WithColor | C2D_WordWrap, *button->font);
+
+    if(button->disabled == 1)
+    {
+        C2D_DrawSpriteTinted(&button->sprite, &disabledTint);
+    }
+
+    // Reset positioning if applicable
+    if(needShadow) {
+        button->sprite.params.pos.x += distance;
+        button->sprite.params.pos.y += distance;
+    }
+
+}
+
+void initTint()
+{
+    u32 shadowColor = C2D_Color32(0x00, 0x00, 0x00, 0xFF);
+    u32 disabledColor = C2D_Color32(0x00, 0x00, 0x00, 0x99);
+
+    for (int i = 0; i < 4; i++) {
+        shadowTint.corners[i].color = shadowColor;
+        shadowTint.corners[i].blend = 1.0f;
+
+        disabledTint.corners[i].color = disabledColor;
+        disabledTint.corners[i].blend = 1.0f;
+    }
+}
+
 int main(int argc, char **argv)
 {
     romfsInit();
     cfguInit();
     gfxInitDefault();
+    initTint();
     atexit(gfxExit);
+
+    // State initialization
+    mainState.rainbowDelayDefault = 5;
+    mainState.rainbowDelay = 5;
 
     // Load graphics
 	spriteSheet = C2D_SpriteSheetLoad("romfs:/gfx/sprites.t3x");
@@ -70,9 +180,15 @@ int main(int argc, char **argv)
 
 	UIButton buttonMain;
 	buttonMain.pressed = 0;
+	buttonMain.disabled = 1;
+	buttonMain.rainbow = 0;
+	buttonMain.text = "CONNECT!";
 	C2D_SpriteFromSheet(&buttonMain.sprite, spriteSheet, 0);
 	buttonMain.sprite.params.pos.x = 38;
-	buttonMain.sprite.params.pos.y = 20;
+	buttonMain.sprite.params.pos.y = 28;
+	buttonMain.textOffsetX = 20;
+	buttonMain.textOffsetY = 0;
+	buttonMain.textScale = 2.2;
 
 
     C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
@@ -85,6 +201,7 @@ int main(int argc, char **argv)
 	touchPosition touch;
 
 	C2D_Font font = C2D_FontLoad("romfs:/ffbold.bcfnt");
+	buttonMain.font = &font;
 	u32 backgroundColor = C2D_Color32(0xF1, 0xEA, 0xA7, 0xFF);
 	u32 textColor  = C2D_Color32(0xFB, 0xFC, 0xFC, 0xFF);
 
@@ -96,6 +213,9 @@ int main(int argc, char **argv)
 
         u32 kDown = hidKeysDown();
 		if (kDown & KEY_START) break; // break in order to return to hbmenu
+		if (kDown & KEY_A) {
+		  buttonMain.disabled = !buttonMain.disabled;
+		}
 
 
 		// Top Screen
@@ -110,29 +230,11 @@ int main(int argc, char **argv)
         // Bottom Screen
         C2D_TargetClear(bottom, backgroundColor);
         C2D_SceneBegin(bottom);
-        C2D_DrawSprite(&buttonMain.sprite);
+        drawButton(&buttonMain);
+        updateRainbowTint(&rainbowTint, &mainState);
 
 		C3D_FrameEnd(0);
 	}
 	C2D_FontFree(font);
 	exit(0);
-}
-
-void failExit(const char *fmt, ...) {
-    va_list ap;
-
-    printf("\x1b[31;1m"); // Set color to red
-    va_start(ap, fmt);
-    vprintf(fmt, ap);
-    va_end(ap);
-    printf("\x1b[0m"); // Reset color
-    printf("\nPress B to exit\n");
-
-    while (aptMainLoop()) {
-        gspWaitForVBlank();
-        hidScanInput();
-
-        u32 kDown = hidKeysDown();
-        if (kDown & KEY_B) exit(0);
-    }
 }
